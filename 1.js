1ac9c39bbf2c0f98a2ea950a8b639f5cd4750706
ddtruong619 <truongdo619@gmail.com>
2018-05-31 15:27:34 +0700

    thay doi lan 3

---
+++ 1.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function()
+{
+	var kt = true;
+	$("#1").click(function()
+	{
+		if (kt == true)
+		{
+		$("#one").addClass("appear");
+		kt = false;
+		return false;
+		}
+		else	
+		{
+		$("#one").removeClass("appear");
+		kt = true;
+		return false;
+		}	
+	})
+})
