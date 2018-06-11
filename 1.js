7a16314f759249fcebacf2597597e18bef8c6244
ddtruong619 <truongdo619@gmail.com>
2018-06-11 18:58:52 +0700

    change all

--- 1.js
+++ 1.js
@@ -1,19 +1,4 @@
-﻿$(document).ready(function()
-{
-	var kt = true;
-	$("#1").click(function()
-	{
-		if (kt == true)
-		{
-		$("#one").addClass("appear");
-		kt = false;
-		return false;
-		}
-		else	
-		{
-		$("#one").removeClass("appear");
-		kt = true;
-		return false;
-		}	
-	})
-})
+﻿ $(function(){
+	 
+})  
+ 
